56f131c69130622ea66e77f6743de9ad703fece2
Jon Ander Besga <jonan.bsg@gmail.com>
2015-04-18 15:50:53 +0200

    Fixed several bugs. No algorithm implemented yet.

--- murlikitas/euskal/views.py
+++ murlikitas/euskal/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 
@@ -71,60 +71,63 @@ def auth_logout(request):
 
 @login_required
 def preferences(request):
-    up = UserProfile.objects.get(user=request.user)  # Get the UserProfile using the User.
+    if request.user.username == 'admin':
+        return HttpResponse("Admin doesn't have preferences")
+    else:
+        up = UserProfile.objects.get(user=request.user)  # Get the UserProfile using the User.
 
-    if request.method == 'POST':
+        if request.method == 'POST':
 
-        left_choices_form = ChoiceForm(request.POST, prefix='left', up=up)  # Get the info from the form.
-        right_choices_form = ChoiceForm(request.POST, prefix='right', up=up)  # Get the info from the form.
-
-        try:
-            upref = UserPreferences.objects.get(user_profile=up)  # Try to find the UserPreferences from the User.
-
-        except UserPreferences.DoesNotExist:  # If they dont exists...
-            upref = UserPreferences(user_profile=up)  # create it
-            upref.save()  # and save it.
-
-        if left_choices_form.is_valid() and right_choices_form.is_valid():  # Validate the forms
-
-            left_choices = left_choices_form.save(commit=False)  # Save data from the form but don't save it
-            right_choices = right_choices_form.save(commit=False)  # Save data from the form but don't save it
-            left_choices.user_preferences = upref  # Assign the UserPreferences from the User
-            right_choices.user_preferences = upref  # Assign the UserPreferences from the User
-            left_choices.save()
-            right_choices.save()
-
-            # If there were choices saved in the UserPreferences before (The form was created before)...
-            if upref.left_choices is not None:
-                old_lc = upref.left_choices  # save the old choices in another variable...
-            if upref.right_choices is not None:
-                old_rc = upref.right_choices
-
-            upref.left_choices = left_choices  # assign the new choices to the UserPreferences
-            upref.right_choices = right_choices
-            upref.save()  # and save.
+            left_choices_form = ChoiceForm(request.POST, prefix='left', up=up)  # Get the info from the form.
+            right_choices_form = ChoiceForm(request.POST, prefix='right', up=up)  # Get the info from the form.
 
             try:
-                # and delete the old choices. This way is possible to have only two choices objects per UserPreferences.
-                old_lc.delete()
-                old_rc.delete()
-            except UnboundLocalError:
-                pass
+                upref = UserPreferences.objects.get(user_profile=up)  # Try to find the UserPreferences from the User.
 
-            return render(request, 'euskal/preferences.html', {'left_choices_form': left_choices_form,
-                                                               'right_choices_form': right_choices_form,
-                                                               'saved': True})
+            except UserPreferences.DoesNotExist:  # If they dont exists...
+                upref = UserPreferences(user_profile=up)  # create it
+                upref.save()  # and save it.
+
+            if left_choices_form.is_valid() and right_choices_form.is_valid():  # Validate the forms
+
+                left_choices = left_choices_form.save(commit=False)  # Save data from the form but don't save it
+                right_choices = right_choices_form.save(commit=False)  # Save data from the form but don't save it
+                left_choices.user_preferences = upref  # Assign the UserPreferences from the User
+                right_choices.user_preferences = upref  # Assign the UserPreferences from the User
+                left_choices.save()
+                right_choices.save()
+
+                # If there were choices saved in the UserPreferences before (The form was created before)...
+                if upref.left_choices is not None:
+                    old_lc = upref.left_choices  # save the old choices in another variable...
+                if upref.right_choices is not None:
+                    old_rc = upref.right_choices
+
+                upref.left_choices = left_choices  # assign the new choices to the UserPreferences
+                upref.right_choices = right_choices
+                upref.save()  # and save.
+
+                try:
+                    # and delete the old choices. This way is possible to have only two choices objects per UserPreferences.
+                    old_lc.delete()
+                    old_rc.delete()
+                except UnboundLocalError:
+                    pass
+
+                return render(request, 'euskal/preferences.html', {'left_choices_form': left_choices_form,
+                                                                   'right_choices_form': right_choices_form,
+                                                                   'saved': True})
+
+            else:
+                print left_choices_form.errors, right_choices_form.errors
 
         else:
-            print left_choices_form.errors, right_choices_form.errors
+            left_choices_form = ChoiceForm(prefix='left', up=up)  # Create the left_choices form.
+            right_choices_form = ChoiceForm(prefix='right', up=up)  # Create the right_choices form.
 
-    else:
-        left_choices_form = ChoiceForm(prefix='left', up=up)  # Create the left_choices form.
-        right_choices_form = ChoiceForm(prefix='right', up=up)  # Create the right_choices form.
-
-    return render(request, 'euskal/preferences.html', {'left_choices_form': left_choices_form,
-                                                       'right_choices_form': right_choices_form,
-                                                        'saved': False})
+        return render(request, 'euskal/preferences.html', {'left_choices_form': left_choices_form,
+                                                           'right_choices_form': right_choices_form,
+                                                            'saved': False})
 
 
 def status(request):
